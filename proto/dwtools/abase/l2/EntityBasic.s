6018d6402c4f457e7168175607145c5eb16a9a4b
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-04-01 13:41:31 +0300

    update jsdoc

--- proto/dwtools/abase/l2/EntityBasic.s
+++ proto/dwtools/abase/l2/EntityBasic.s
@@ -5,16 +5,11 @@
 /**
  * Collection of routines for processing heterogenous data.
  * @module Tools/base/EntityFundamentals
+ * @extends Tools
  */
 
 /**
  * @file l3/Entity.s.
- */
-
-/**
- * Collection of routines for processing heterogenous data.
- * @namespace Tools( module::EntityFundamentals )
- * @memberof module:Tools/base/EntityFundamentals
  */
 
 if( typeof module !== 'undefined' )
@@ -381,7 +376,7 @@ eachInMultiRange.defaults =
  * _.entityValueWithIndex( { a : 'str', b : 123 }, 2 )
  *
  * @function entityValueWithIndex
- * @memberof module:Tools/base/EntityFundamentals.Tools( module::EntityFundamentals )
+ * @memberofs "module:Tools/base/EntityFundamentals","module:Tools/base/Fundamental.wTools"
 */
 
 function entityValueWithIndex( src, index )
@@ -435,7 +430,7 @@ function entityValueWithIndex( src, index )
  * _.entityKeyWithValue( { a : 'str', b : 123 }, 123 )
  *
  * @function entityKeyWithValue
- * @memberof module:Tools/base/EntityFundamentals.Tools( module::EntityFundamentals )
+ * @memberofs "module:Tools/base/EntityFundamentals","module:Tools/base/Fundamental.wTools"
 */
 
 function entityKeyWithValue( src, value )
@@ -510,7 +505,7 @@ function entityVals( src )
  * @function entityCoerceTo
  * @throws {exception} If only one or no arguments provided.
  * @throws {exception} If type of( ins ) is not supported.
- * @memberof module:Tools/base/EntityFundamentals.Tools( module::EntityFundamentals )
+ * @memberofs "module:Tools/base/EntityFundamentals","module:Tools/base/Fundamental.wTools"
  *
  */
 
@@ -579,7 +574,7 @@ function entityFreeze( src )
  *
  * @function entityHasNan
  * @throws {exception} If no argument provided.
- * @memberof module:Tools/base/EntityFundamentals.Tools( module::EntityFundamentals )
+ * @memberofs "module:Tools/base/EntityFundamentals","module:Tools/base/Fundamental.wTools"
  *
  */
 
@@ -635,7 +630,7 @@ function entityHasNan( src )
  *
  * @function entityHasUndef
  * @throws {exception} If no argument provided.
- * @memberof module:Tools/base/EntityFundamentals.Tools( module::EntityFundamentals )
+ * @memberofs "module:Tools/base/EntityFundamentals","module:Tools/base/Fundamental.wTools"
  *
  */
 
